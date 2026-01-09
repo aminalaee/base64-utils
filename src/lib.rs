65edec6ab7df8a2cb35574fbe66d418d5229bf69
Amin Alaee <mohammadamin.alaee@gmail.com>
2026-01-09 18:04:37 +0100

    add b64encode

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,38 @@
-use base64_simd::STANDARD;
+use base64_simd::{Out, STANDARD};
 use pyo3::prelude::*;
+use pyo3::types::PyBytes;
 
 #[pyfunction]
-pub fn b64encode(s: &[u8]) -> PyResult<String> {
-    Ok(STANDARD.encode_to_string(s))
+#[pyo3(signature = (s, altchars=None))]
+pub fn b64encode(py: Python<'_>, s: &[u8], altchars: Option<&[u8]>) -> PyResult<Py<PyBytes>> {
+    if let Some(alt) = altchars {
+        if alt.len() != 2 {
+            return Err(PyErr::new::<pyo3::exceptions::PyValueError, _>(
+                "altchars must be a bytes object of length 2",
+            ));
+        }
+
+        let output_len = STANDARD.encoded_length(s.len());
+        let mut output = vec![0u8; output_len];
+        let _ = STANDARD.encode(s, Out::from_slice(&mut output));
+
+        for byte in &mut output {
+            if *byte == b'+' {
+                *byte = alt[0];
+            } else if *byte == b'/' {
+                *byte = alt[1];
+            }
+        }
+
+        return Ok(PyBytes::new(py, &output).into());
+    }
+
+    let output_len = STANDARD.encoded_length(s.len());
+    let output = PyBytes::new_with(py, output_len, |buf| {
+        let _ = STANDARD.encode(s, Out::from_slice(buf));
+        Ok(())
+    })?;
+    Ok(output.into())
 }
 
 #[pymodule]

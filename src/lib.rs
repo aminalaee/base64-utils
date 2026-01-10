dc4ae0becbce189b6c4f0c2830504c269b86fc37
Amin Alaee <mohammadamin.alaee@gmail.com>
2026-01-10 09:59:06 +0100

    improve b64encode (#3)

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,35 @@
 use base64_simd::{Out, STANDARD};
+use pyo3::exceptions::PyValueError;
 use pyo3::prelude::*;
 use pyo3::types::PyBytes;
 
 #[pyfunction]
 #[pyo3(signature = (s, altchars=None))]
 pub fn b64encode(py: Python<'_>, s: &[u8], altchars: Option<&[u8]>) -> PyResult<Py<PyBytes>> {
+    let output_len = STANDARD.encoded_length(s.len());
+
     if let Some(alt) = altchars {
         if alt.len() != 2 {
-            return Err(PyErr::new::<pyo3::exceptions::PyValueError, _>(
-                "altchars must be a bytes object of length 2",
+            return Err(PyValueError::new_err(
+                "altchars must be a bytes-like object of length 2",
             ));
         }
 
-        let output_len = STANDARD.encoded_length(s.len());
-        let mut output = vec![0u8; output_len];
-        let _ = STANDARD.encode(s, Out::from_slice(&mut output));
+        let output = PyBytes::new_with(py, output_len, |buf| {
+            let _ = STANDARD.encode(s, Out::from_slice(buf));
 
-        for byte in &mut output {
-            if *byte == b'+' {
-                *byte = alt[0];
-            } else if *byte == b'/' {
-                *byte = alt[1];
+            for byte in buf.iter_mut() {
+                *byte = match *byte {
+                    b'+' => alt[0],
+                    b'/' => alt[1],
+                    b => b,
+                };
             }
-        }
-
-        return Ok(PyBytes::new(py, &output).into());
+            Ok(())
+        })?;
+        return Ok(output.into());
     }
 
-    let output_len = STANDARD.encoded_length(s.len());
     let output = PyBytes::new_with(py, output_len, |buf| {
         let _ = STANDARD.encode(s, Out::from_slice(buf));
         Ok(())

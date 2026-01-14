58f1c352d0ada86f1051c78efbe00c16315fb366
Amin Alaee <mohammadamin.alaee@gmail.com>
2026-01-14 15:08:06 +0100

    Add `standard_b64decode` and `urlsafe_b64decode` (#6)

--- python/base64_utils/__init__.py
+++ python/base64_utils/__init__.py
@@ -1,13 +1,17 @@
 from ._base64_utils import (
     b64decode,
     b64encode,
+    standard_b64decode,
     standard_b64encode,
+    urlsafe_b64decode,
     urlsafe_b64encode,
 )
 
 __all__ = [
-    "b64encode",
     "b64decode",
+    "b64encode",
+    "standard_b64decode",
     "standard_b64encode",
+    "urlsafe_b64decode",
     "urlsafe_b64encode",
 ]
